58f99ebd6e977a4eaca22ee7231ad805c6f5b7bc
akagi <hoaiphuong.nguyen.vn@gmail.com>
2019-07-20 18:32:24 +0700

    Navigator bar

---
+++ Navigator-Bar/index.js
@@ -0,0 +1,20 @@
+function home() {
+    document.getElementById("root").innerHTML = "Welcome to my website"
+}
+
+const news = () => {
+    document.getElementById("root").innerHTML =
+    "Vietnam and China have a big war on East Sea"
+}
+
+const contact = () => {
+    document.getElementById("root").innerHTML =
+    "Please contact me via:" + "<br />" +
+    "Mail: hoaiphuong.nguyen.vn@gmail.com" + "<br />" +
+    "Phone: +84 938 038 621"
+}
+
+const about = () => {
+    document.getElementById("root").innerHTML =
+    "I am a developer"
+}
